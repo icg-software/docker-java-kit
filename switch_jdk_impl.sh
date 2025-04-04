6d723188cba1b3ed70fba31e33e71b7b6bbdd2af
Jonas Liepe <j_liepe@icg-software.de>
2025-04-04 11:25:35 +0200

    fix update-alternatives

--- switch_jdk_impl.sh
+++ switch_jdk_impl.sh
@@ -10,6 +10,8 @@ RHEL_OPENJDK_RELEASE=
 case "$1" in
     oraclejdk)
         echo "Set JDK Implementation to OracleJDK"
+        alternatives --install /usr/bin/javac javac /usr/java/jdk1.${JAVA_VERSION_MAJOR}.0_${JAVA_VERSION_MINOR}-amd64/bin/javac 2
+        alternatives --install /usr/bin/java java /usr/java/jdk1.${JAVA_VERSION_MAJOR}.0_${JAVA_VERSION_MINOR}-amd64/jre/bin/java 2
         alternatives --set javac /usr/java/jdk1.${JAVA_VERSION_MAJOR}.0_${JAVA_VERSION_MINOR}-amd64/bin/javac
         alternatives --set java /usr/java/jdk1.${JAVA_VERSION_MAJOR}.0_${JAVA_VERSION_MINOR}-amd64/jre/bin/java
         rm /opt/jdk/latest
@@ -18,6 +20,8 @@ case "$1" in
 
     openjdk)
         echo "Set JDK Implementation to OpenJDK"
+        alternatives --install /usr/bin/javac javac /usr/lib/jvm/${RHEL_OPENJDK_PKG_NAME}-${RHEL_OPENJDK_VERSION}-${RHEL_OPENJDK_RELEASE}.x86_64/bin/javac 2
+        alternatives --install /usr/bin/java java /usr/lib/jvm/${RHEL_OPENJDK_PKG_NAME}-${RHEL_OPENJDK_VERSION}-${RHEL_OPENJDK_RELEASE}.x86_64/jre/bin/java 2
         alternatives --set javac /usr/lib/jvm/${RHEL_OPENJDK_PKG_NAME}-${RHEL_OPENJDK_VERSION}-${RHEL_OPENJDK_RELEASE}.x86_64/bin/javac
         alternatives --set java /usr/lib/jvm/${RHEL_OPENJDK_PKG_NAME}-${RHEL_OPENJDK_VERSION}-${RHEL_OPENJDK_RELEASE}.x86_64/jre/bin/java
         rm /opt/jdk/latest
